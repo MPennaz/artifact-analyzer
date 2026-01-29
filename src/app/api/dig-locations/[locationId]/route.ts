049e6dd1cfa82391060b4e2f56b5ced27c7b801c
Matt Pennaz <mpennaz@gmail.com>
2026-01-29 13:14:09 -0600

    Fix dig-locations route module exports

--- src/app/api/dig-locations/[locationId]/route.ts
+++ src/app/api/dig-locations/[locationId]/route.ts
@@ -1,49 +1,9 @@
 // src/app/api/dig-locations/[locationId]/route.ts
+import { jsonOk, jsonError, withAuthApi } from '@/lib/api/with-auth';
 
-import { z } from 'zod';
-import { withAuthApi, jsonOk, jsonError } from '@/lib/api/with-auth';
-
-// TEMP: until your dig-locations feature is wired
-const DigLocationIdParamSchema = z.object({
-  locationId: z.string().uuid()
-});
-
-type Ctx = { params: Promise<{ locationId: string }> | { locationId: string } };
-
-export async function GET(_req: Request, ctx: Ctx) {
+export async function GET(_req: Request) {
   try {
-    const { locationId } = DigLocationIdParamSchema.parse(await ctx.params);
-
-    return await withAuthApi(async () => {
-      // TODO: hook up digLocationsServices.getById(...)
-      return jsonOk({ location: { id: locationId } });
-    });
-  } catch (err) {
-    return jsonError(err);
-  }
-}
-
-export async function PATCH(_req: Request, ctx: Ctx) {
-  try {
-    const { locationId } = DigLocationIdParamSchema.parse(await ctx.params);
-
-    return await withAuthApi(async () => {
-      // TODO: hook up digLocationsServices.update(...)
-      return jsonOk({ location: { id: locationId } });
-    });
-  } catch (err) {
-    return jsonError(err);
-  }
-}
-
-export async function DELETE(_req: Request, ctx: Ctx) {
-  try {
-    const { locationId } = DigLocationIdParamSchema.parse(await ctx.params);
-
-    return await withAuthApi(async () => {
-      // TODO: hook up digLocationsServices.remove(...)
-      return jsonOk({ ok: true, locationId });
-    });
+    return await withAuthApi(async () => jsonOk({ ok: true }));
   } catch (err) {
     return jsonError(err);
   }
